0d5e6cb92e46d552ab7c9b2f357173a2c038e810
gatch14 <gatch14@gmail.com>
2018-04-16 15:41:26 +0200

    add handling CORS and body-parser

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./shop/routes/products');
 const orderRoutes = require('./shop/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+		return res.status(200).json({});
+	}
+	next();
+});
 
 // Routes
 app.use('/products', productRoutes);
