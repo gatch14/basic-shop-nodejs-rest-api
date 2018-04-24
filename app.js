cddc20c61de42d5e7cfd57e4468c0dd9587be018
gatch14 <gatch14@gmail.com>
2018-04-24 15:29:01 +0200

    add route protection

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+app.use(bodyParser.json({limit: '50mb'}));
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 	res.status(error.status || 500);
 	res.json({
 		error: {
-			message: error.message
+			message: "erreur 500 app " + error.message
 		}
 	});
 });
--- shop/routes/products.js
+++ shop/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
 	destination: function(req, file, cb) {
@@ -67,7 +68,8 @@ router.get('/', (req, res, next) => {
 		});
 });
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
+
 	const product = new Product({
 		_id: new mongoose.Types.ObjectId(),
 		name: req.body.name,
@@ -76,7 +78,7 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
 	});
 	product
 		.save()
-		.then(result => {			
+		.then(result => {		
 			res.status(201).json({
 				message: 'Created product success',
 				createProduct: {
@@ -91,7 +93,6 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
 			});
 		})
 		.catch(err => {
-			console.log(err);
 			res.status(500).json({
 				error: err
 			});
@@ -104,7 +105,6 @@ router.get('/:productId', (req, res, next) => {
 		.select('name price _id productImage')
 		.exec()
 		.then(result => {
-			console.log(result);
 			if (result) {
 				res.status(200).json({
 					product: result,
@@ -126,7 +126,7 @@ router.get('/:productId', (req, res, next) => {
 		});
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
 	const id = req.params.productId;	
 	const update = req.body;
 
@@ -156,7 +156,7 @@ router.patch('/:productId', (req, res, next) => {
 	});
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
 	const id = req.params.productId;
 
 	Product.remove({ _id: id})
